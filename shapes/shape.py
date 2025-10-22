4d6b59f082701765871fc752e7b8c2d39f7a10b8
Roniel David Castro <rocastron@unal.edu.co>
2025-10-21 19:36:49 -0500

    Update shape.py

--- shapes/shape.py
+++ shapes/shape.py
@@ -1,1 +1,27 @@
 
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    def __init__(self, name):
+        self._name = name  
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        self._name = name
+
+    @abstractmethod
+    def compute_area(self):
+        pass
+
+    @abstractmethod
+    def compute_perimeter(self):
+        pass
+
+    @abstractmethod
+    def compute_inner_angles(self):
+        pass
+
+    @abstractmethod
+    def is_it_regular(self):
+        pass
