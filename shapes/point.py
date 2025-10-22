4a6f1358abcc0e5adc210e116752a7bf7d41cf1a
Roniel David Castro <rocastron@unal.edu.co>
2025-10-21 19:38:08 -0500

    Create point.py

---
+++ shapes/point.py
@@ -0,0 +1,20 @@
+
+class Point:
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+      
+    def get_x(self):
+        return self._x
+
+    def set_x(self, x):
+        self._x = x
+
+    def get_y(self):
+        return self._y
+
+    def set_y(self, y):
+        self._y = y
+
+    def __repr__(self):
+        return f"({self._x}, {self._y})"
