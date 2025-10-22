f05186a1324d6ef74fe44f1866de719774413fc4
Roniel David Castro <rocastron@unal.edu.co>
2025-10-21 19:47:30 -0500

    Update rectangle.py

--- shapes/rectangle.py
+++ shapes/rectangle.py
@@ -1,27 +1,36 @@
-
 from .shape import Shape
-from .point import Point
+from .line import Line
 
 class Rectangle(Shape):
-    def __init__(self, p1: Point, p2: Point):
-        super().__init__("Rectangle")
-        self._p1 = p1
-        self._p2 = p2
+    def __init__(self, point1, point2):
+        super().__init__()
+        self.__point1 = point1
+        self.__point2 = point2
+        self.__width = abs(point2.get_x() - point1.get_x())
+        self.__height = abs(point2.get_y() - point1.get_y())
+
+    def get_width(self):
+        return self.__width
+
+    def set_width(self, w):
+        if w > 0:
+            self.__width = w
+
+    def get_height(self):
+        return self.__height
+
+    def set_height(self, h):
+        if h > 0:
+            self.__height = h
 
     def compute_area(self):
-        width = abs(self._p2.get_x() - self._p1.get_x())
-        height = abs(self._p2.get_y() - self._p1.get_y())
-        return width * height
+        return self.__width * self.__height
 
     def compute_perimeter(self):
-        width = abs(self._p2.get_x() - self._p1.get_x())
-        height = abs(self._p2.get_y() - self._p1.get_y())
-        return 2 * (width + height)
+        return 2 * (self.__width + self.__height)
 
     def compute_inner_angles(self):
         return [90, 90, 90, 90]
 
     def is_it_regular(self):
-        width = abs(self._p2.get_x() - self._p1.get_x())
-        height = abs(self._p2.get_y() - self._p1.get_y())
-        return width == height
+        return self.__width == self.__height
