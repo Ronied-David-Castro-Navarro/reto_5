4834d05e81701ce4195b801b52ca9219f11334c1
Roniel David Castro <rocastron@unal.edu.co>
2025-10-21 19:36:13 -0500

    Update __init__.py

--- shapes/__init__.py
+++ shapes/__init__.py
@@ -1,1 +1,10 @@
 
+from .point import Point
+from .line import Line
+from .shape import Shape
+from .rectangle import Rectangle
+from .square import Square
+from .triangle_equilateral import TriangleEquilateral
+from .triangle_isosceles import TriangleIsosceles
+from .triangle_scalene import TriangleScalene
+from .triangle_rectangle import TriangleRectangle
